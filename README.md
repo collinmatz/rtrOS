999b0ac1799f75a453ba7b4f162e14ddbc9bfb15
Collin Matz <collin.matz.a@gmail.com>
2024-03-06 21:08:16 -0600

    Added absolute bare bones necessity for Rust binary to run

--- README.md
+++ README.md
@@ -6,3 +6,14 @@
 <p align="center">
   A real time Rust OS.
 </p>
+<br>
+<br>
+
+<hr>
+<p>
+  Useful Links:
+  <br>
+  Tutorial: https://os.phil-opp.com/
+  <br>
+  Style Guide: https://doc.rust-lang.org/nightly/style-guide/
+</p>
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+// main.rs
+
+// don't link the Rust standard library
+#![no_std] 
+// disable all Rust-level entry points
+#![no_main]
+
+use core::panic::PanicInfo;
+
+// this function is called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+// don't mangle the name of this function
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for the function
+    // named `_start` by default
+    loop {}
 }
